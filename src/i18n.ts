41d93760dd35192a59ebcfda2faef1a78a242e48
ural <sevomax@gmail.com>
2025-11-25 18:21:26 -0500

    fixing errors

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,12 +4,15 @@ import {getRequestConfig} from 'next-intl/server';
 // Can be imported from a shared config
 const locales = ['en', 'ru'];
  
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async ({requestLocale}) => {
+  // `requestLocale` is a Promise that resolves to the matched locale segment
+  const locale = await requestLocale;
+
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
- 
+  if (!locale || !locales.includes(locale as any)) notFound();
+
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
